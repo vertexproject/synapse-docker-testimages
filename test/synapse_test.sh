3512215c3629871b72289980f087a9503d862a63
epiphyte <epiphyte@vertex.link>
2017-11-05 14:38:02 -0500

    Do Travis tests on python 3.6 again.  Fail tests faster.

--- test/synapse_test.sh
+++ test/synapse_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 git clone https://github.com/vertexproject/synapse.git  ./synapse
 cd ./synapse
-pytest -v -s --disable-warnings --durations 6 --cov synapse --no-cov-on-fail --cov-report=term
+pytest -v -s --disable-warnings --durations 6 --maxfail 6 --cov synapse --no-cov-on-fail --cov-report=term
 exit $?
